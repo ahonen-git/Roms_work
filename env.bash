33eabc9675e48014b82d8be453f4b5ee8bb01d66
ahonen-git <misumi@criepi.denken.or.jp>
2016-07-05 09:40:31 +0900

    Correct path to gridid.txt.

--- env.bash
+++ env.bash
@@ -2,5 +2,5 @@
 
 export PYTHONPATH=/Users/misumi/python:/usr/local/lib/python2.7/site-packages
 export PYROMS=$PYTHONPATH/pyroms
-export PYROMS_GRIDID_FILE=/Users/misumi/work/160701_roms_work/gridid.txt
+export PYROMS_GRIDID_FILE=/Users/misumi/tower/Roms_work/gridid.txt
 export PYTHONSTARTUP=$HOME/.pythonstartup
